4ca6ff6d401cc173212de34ea99e534b41d21c2e
Agnes Romhanyi <romhanyi.agi@gmail.com>
2022-01-28 09:11:11 -0800

    Simple composite pattern

---
+++ CompositePattern/CompositePattern/CompositePart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositePattern
+{
+    public class CompositePart : IComponent
+    {
+        private List<IComponent> componentList = new List<IComponent>();
+        private float price;
+        private string name;
+
+        public CompositePart(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddComponent(IComponent component)
+        {
+            componentList.Add(component);
+        }
+
+        public void RemoveHerd(IComponent component)
+        {
+            componentList.Remove(component);
+        }
+
+        public float GetSalesPrice()
+        {
+            price = 0;
+            foreach (IComponent component in componentList)
+            {
+               price += component.GetSalesPrice();
+            }
+            return price;
+        }
+
+        public string PrintComponent()
+        {
+            string components = "";
+            foreach (IComponent component in componentList)
+            {
+                components += component.PrintComponent();
+            }
+            return name + " [" + components + "]";
+        }
+    }
+}
---
+++ CompositePattern/CompositePattern/Part.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CompositePattern
+{
+    public class Part : IComponent
+    {
+        private float price;
+        private string name;
+
+        public Part(float price, string n)
+        {
+            this.price = price;
+            name = n;
+        }
+
+        public float GetSalesPrice() {
+            return price;
+        }
+
+        public string PrintComponent()
+        {
+            return name + ", ";
+        }
+    }
+}
